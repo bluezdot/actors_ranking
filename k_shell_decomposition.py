d7054b00870fbd3c0b61950e839ee231a35580b4
Truongpham <truong@truongpham.local>
2023-02-13 19:02:57 +0700

    update algorithm

--- k_shell_decomposition.py
+++ k_shell_decomposition.py
@@ -2,24 +2,43 @@
 # 0.Import required modules
 import networkx as nx
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
 
-edges = [("Lập", "Nhật"), ("Lập", "Trường"), ("Trường", "Hưng"), ("Trường", "Khánh"),
-    ("Tùng", "Khánh"), ("Huy", "Khánh"), ("Huy", "Tâm"), ("Tuấn", "Trường"), ("Tuấn", "Khánh")]
+
+# edges = [("Lập", "Nhật"), ("Lập", "Trường"), ("Trường", "Hưng"), ("Trường", "Khánh"),
+#     ("Tùng", "Khánh"), ("Huy", "Khánh"), ("Huy", "Tâm"), ("Tuấn", "Trường"), ("Tuấn", "Khánh"),
+#     ("Rosé", "Trường"), ("Rosé", "Khánh")]
 
 # 1.Create graph object
-def create_graph(edges : list):
+def create_edges():
+        datafile = open('./popularity_weight.csv')
+        edges = []
+        for line in datafile:
+            row_data = line.strip("\n").split(",")
+            row_data = row_data[:2]
+            
+            # try:
+            #     row_data[2] = float(row_data[2])
+            # except ValueError:
+            #     pass
+            edges.append(row_data)
+        edges.pop(0)
+        return edges
+    
+def create_graph(edges):
     graph = nx.Graph()
     graph.add_edges_from(edges)
     return graph
   
 # 2.Check if there is any node left with degree d
 def check(graph_copy, d):
-    f = 0  # there is no node of deg <= d
+    is_choose = 0  # there is no node of deg <= d
     for i in graph_copy.nodes():
         if (graph_copy.degree(i) <= d):
-            f = 1
+            is_choose = 1
             break
-    return f
+    return is_choose
   
   
 # 3.Find list of nodes with particular degree
@@ -30,16 +49,14 @@ def find_nodes(graph_copy, it):
             set1.append(i)
     return set1
 
-
+# 4.K shell decomposition
 def k_shell_decomposition():
-    graph = create_graph(edges)
+    graph = create_graph(create_edges())
     graph_copy = graph.copy()
     
     it = 1  
-    # Bucket being filled currently
-    tmp = []
-    # list of lists of buckets
-    buckets = []
+    tmp = [] # Bucket being filled currently
+    buckets = [] # list of lists of buckets
     while (1):
         flag = check(graph_copy, it)
         if (flag == 0):
@@ -57,8 +74,13 @@ def k_shell_decomposition():
     return buckets
 
 # 5.Get K Shell Decomposition
-print(k_shell_decomposition())
-  
+def main():
+    print(k_shell_decomposition())
+
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
+
 # 6.Visualization
-nx.draw(create_graph(edges), with_labels=1)
-plt.show()
+# nx.draw(create_edges(), with_labels=1)
+# plt.show()
